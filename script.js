16ab62b8e46c94d0d354e58103c456710d4684dd
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-22 21:46:07 +0000

    added readme file

--- script.js
+++
@@ -1,7 +0,0 @@
-// click start button to start countdown timer
-// load first question
-// when an answer is clicked, let the next question load
-// if the answer is incorrect, substract time from clock to increase stress levels, because we like to be mean
-// end quiz if all answers are finished or the clock is, which ever comes first
-// at game end display score
-// give option to save initials and score
